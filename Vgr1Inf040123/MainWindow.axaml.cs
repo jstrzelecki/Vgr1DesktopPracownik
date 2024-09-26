e65c27450fc2ac970b61cefb4b6c66a0dbec0d89
js <jareks@com.pl>
2024-09-26 08:06:51 +0200

    Make GUI

---
+++ Vgr1Inf040123/MainWindow.axaml.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Vgr1Inf040123;
+
+public partial class MainWindow : Window
+{
+    private readonly string smallChars = "qwertyuiopasdfghjklzxcvbnm";
+    private readonly string capitalChars = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    private readonly string numbers = "0123456789";
+    private readonly string specialCharacters = "!@#$%^&*()_+{}[]<>|";
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void GeneratePassword(object? sender, RoutedEventArgs e)
+    {
+        int charNum;
+        try
+        {
+            charNum = int.Parse(CharsNum.Text!.ToString());
+        }
+        catch (Exception)
+        {
+            charNum = 0;
+        }
+        
+        bool isBothLetters = BothLetters.IsChecked ?? false;
+        bool isNumbers = Numbers.IsChecked ?? false;
+        bool isSpecialChars = SpecialChars.IsChecked ?? false;
+        
+        
+        
+        
+        Random rnd = new();
+        
+        // rnd
+        // if (isBothLetters)
+
+        
+    }
+
+    private void ApplyPassword(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
